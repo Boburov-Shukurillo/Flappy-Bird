7ba653b262a7d619337d0ed6385c332b82604934
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2024-03-15 13:20:41 +0500

    inital commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector("canvas");
+const innerWidth = 500;
+const innerHeight = 640;
+const ctx = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+
